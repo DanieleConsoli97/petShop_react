9e2f56dba4a3e47fe97e08b36ba13ee3ea17e984
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-03-29 18:45:32 +0100

    add exemple hero image

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,13 +1,14 @@
 const Hero = () => {
     return (
-        <div className="px-4 py-5  text-center bg-secondary">
-            <h1 className="display-5 fw-bold">Centered hero</h1>
+        <div className="back-img  px-4 py-5  text-center bg-secondary">
+          <div className="my-5">
+            <h1 className="display-5 fw-bold">Tutto per i tuoi amici a quatto zampe</h1>
             <div className="col-lg-6 mx-auto">
-                <p className="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                <p className="lead mb-4">Dai al tuo amico a quattro zampe il meglio che merita con la nostra selezione premium di cibo, accessori, giochi e cure. Che tu abbia un cucciolo vivace o un gatto sofisticato, qui trovi tutto ciò che serve per la sua felicità e benessere.</p>
                 <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                    <button type="button" className="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
-                    <button type="button" className="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+                    
                 </div>
+            </div>
             </div>
         </div>
     )
